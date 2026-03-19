604a212202716e3f807dad98e5daa50526e99bc6
Sparshtrip <sparshtripathi27@gmail.com>
2026-03-19 23:13:52 +0530

    Add files via upload

--- MAX_NUM_FUNCTION.py
+++ MAX_NUM_FUNCTION.py
@@ -1,24 +1,12 @@
-
-def find_maximum(numbers):
-
-    max_num = numbers[0]
-    min_num = numbers[0]
-
-    for num in numbers:
+class Solution:
+    def getMinMax(self, arr):
+        mini = arr[0]
+        maxi = arr[0]
         
-
-        if num > max_num:
-            max_num = num
-            
-        elif num<min_num:
-            min_num= num
-            print(min_num)
-
-    return max_num,min_num
-
-numbers = [12, 45, 23, 67, 34, 89, 10]
-
-maximum_value,minimum_value = find_maximum(numbers)
-
-print("The maximum value in the list is:",maximum_value )
-print("The minimum value in the list is:",minimum_value )
+        for num in arr:
+            if num < mini:
+                mini = num
+            if num > maxi:
+                maxi = num
+                
+        return [mini, maxi]
